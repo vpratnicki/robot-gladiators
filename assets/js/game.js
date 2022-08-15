661d2b541b9438514d27819a75f6d3569e006ba6
vpratnicki <vanessa@pratnicki.net>
2022-08-14 22:55:14 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -41,14 +41,16 @@ var fight = function(enemyName) {
     if (confirmSkip) {
       window.alert(playerName + " has decided to skip this fight. Goodbye!");
       // subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 10;
+      playerMoney = Math.max(0, playerMoney - 10);
       console.log("playerMoney", playerMoney);
       break;
     }
   }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " healh remaining.");
 
     // Check enemy's health
@@ -64,7 +66,9 @@ var fight = function(enemyName) {
     }
     
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining,");
 
     // check player's health
@@ -97,7 +101,7 @@ var fight = function(enemyName) {
     var pickedEnemyName = enemyNames [i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40,60);
 
     // use debugger to pause script from running and check what's going on at that moment in the code
     // debugger;
@@ -196,6 +200,12 @@ var shop = function() {
   }
 };
 
+    // function to generate a random numeric value
+    var randomNumber = function(min, max) {
+      var value = Math.floor(Math.random() * (max - min + 1) + min);
+    
+      return value;
+    };
 
 
 // start the game when the page loads
